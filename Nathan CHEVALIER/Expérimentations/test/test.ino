b35121a175712d390f6186625e2c5b0f1b766740
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2018-01-14 19:46:32 +0100

    Après Séance du 12/01/18

---
+++ Nathan CHEVALIER/Expérimentations/test/test.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include "Ultrasonic.h"
+
+rgb_lcd lcd;
+Ultrasonic ultrasonic(7);
+
+void setup()
+{
+    Serial.begin(9600);
+    lcd.begin(16, 2);
+    lcd.setRGB(0, 255, 0);
+}
+
+void loop()
+{
+    long distance;
+
+    distance = ultrasonic.MeasureInCentimeters();
+    Serial.print(distance);
+    Serial.println(" cm");
+    
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.setRGB(0,0,255);
+    lcd.print(distance);
+    
+    delay(200);
+}
+
