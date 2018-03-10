bcd09385f219897de1ee01813ba572a412499bec
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2018-03-10 12:55:49 +0100

    Sauvegarde 10/03

--- Nathan CHEVALIER/Expérimentations/test/test.ino
+++ Nathan CHEVALIER/Expérimentations/test/test.ino
@@ -1,30 +1,66 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
+//#include <Digital_Light_TSL2561.h>
 #include "Ultrasonic.h"
 
 rgb_lcd lcd;
 Ultrasonic ultrasonic(7);
+Ultrasonic ultrasonic2(8);
+//RGB sur port I2C
+//1 Ultrasons sur D7
+//2 Ultrason sur D8
+//Photorésistance sur D2
+float Rsensor;
+long distance1;
+long distance2;
+int distances1[] = {0};
+int distances2[] = {0};
+int newcase;
 
 void setup()
 {
-    Serial.begin(9600);
+    Wire.begin();
     lcd.begin(16, 2);
-    lcd.setRGB(0, 255, 0);
+    lcd.setRGB(6, 247, 238);
+    Serial.begin(9600);
+    //TSL2561.init();
 }
 
 void loop()
-{
-    long distance;
+{  
+    distance1 = ultrasonic.MeasureInCentimeters();
+    delay(10);
+    distance2 = ultrasonic2.MeasureInCentimeters();
 
-    distance = ultrasonic.MeasureInCentimeters();
-    Serial.print(distance);
-    Serial.println(" cm");
-    
-    lcd.clear();
-    lcd.setCursor(0, 1);
-    lcd.setRGB(0,0,255);
-    lcd.print(distance);
-    
-    delay(200);
+    //On détecte un changements par rapport à la distance initiale
+    if(distance1 < 40 || distance2 < 40)
+    {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print(distance1);
+        lcd.setCursor(0,1);
+        lcd.print(distance2);
+        newcase = sizeof(distance1);
+        Serial.print('Nouvelle case: ');
+        Serial.print(newcase);
+        Serial.print('\n');
+        distances1[newcase + 1] = distance1;
+        for(int i = 0; i <= sizeof(distances1); i++)
+        {
+            Serial.print(distances1[i]);
+            Serial.print('-');
+        }
+    }
+    else
+    {
+        lcd.clear();
+    }
+
+  //TSL2561.readVisibleLux()
+
+    Serial.print(sizeof(distances1));
+    Serial.print('\n');
+
+    delay(100);
 }
 
